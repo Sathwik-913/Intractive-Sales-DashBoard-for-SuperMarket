3473310db4560cc8a3094cd9038227625e03278a
Sathwik-913 <136552212+Sathwik-913@users.noreply.github.com>
2024-08-27 21:27:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
 import pandas as pd 
 import plotly.express as px  
 import streamlit as st
-# emojis: https://www.webfx.com/tools/emoji-cheat-sheet/
 st.set_page_config(page_title="Sales Dashboard", page_icon=":bar_chart:", layout="wide")
-# ---- READ EXCEL ----
 @st.cache_data
 def get_data_from_excel():
     df = pd.read_excel(
@@ -14,11 +12,9 @@ def get_data_from_excel():
         usecols="B:R",
         nrows=1000,
     )
-    # Add 'hour' column to dataframe
     df["hour"] = pd.to_datetime(df["Time"], format="%H:%M:%S").dt.hour
     return df
 df = get_data_from_excel()
-# ---- SIDEBAR ----
 st.sidebar.header("Please Filter Here:")
 city = st.sidebar.multiselect(
     "Select the City:",
@@ -42,10 +38,8 @@ df_selection = df.query(
 if df_selection.empty:
     st.warning("No data available based on the current filter settings!")
     st.stop() # This will halt the app from further execution.
-# ---- MAINPAGE ----
 st.title(":bar_chart: Sales Dashboard")
 st.markdown("##")
-# TOP KPI's
 total_sales = int(df_selection["Total"].sum())
 average_rating = round(df_selection["Rating"].mean(), 1)
 star_rating = ":star:" * int(round(average_rating, 0))
@@ -76,7 +70,6 @@ fig_product_sales.update_layout(
     plot_bgcolor="rgba(0,0,0,0)",
     xaxis=(dict(showgrid=False))
 )
-# SALES BY HOUR [BAR CHART]
 sales_by_hour = df_selection.groupby(by=["hour"])[["Total"]].sum()
 fig_hourly_sales = px.bar(
     sales_by_hour,
@@ -94,7 +87,6 @@ fig_hourly_sales.update_layout(
 left_column, right_column = st.columns(2)
 left_column.plotly_chart(fig_hourly_sales, use_container_width=True)
 right_column.plotly_chart(fig_product_sales, use_container_width=True)
-# ---- HIDE STREAMLIT STYLE ----
 hide_st_style = """
             <style>
             #MainMenu {visibility: hidden;}
